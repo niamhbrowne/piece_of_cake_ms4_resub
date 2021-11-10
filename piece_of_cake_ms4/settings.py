79e3593bb025d6e3ce0fe1ce7a9e5574d1fa0501
niamhbrowne <niamh_browne@icloud.com>
2021-11-10 11:27:24 +0000

    added secret key

--- piece_of_cake_ms4/settings.py
+++ piece_of_cake_ms4/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_4@e!-f=+a8lqhn3k5z8w&nvna&&yiv*pm8b!t%e*707p)35+^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['niamhbrowne-pieceofcake-ms4.herokuapp.com', 'localhost']
 
